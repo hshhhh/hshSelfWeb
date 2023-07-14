814dab9eb1ff3152b983f40e538bd936f44cbf4f
huangshenghua <huangshenghua@dianxiaomi.com>
2023-07-14 17:41:27 +0800

    feat: 初始化项目

---
+++ README.md
@@ -0,0 +1,7 @@
+### 黄盛华个人主页 1.0
+
+基于 GitHub 上严海翔（海怪）的个人项目（[me](https://github.com/haixiangyan/me)）修改
+
+侵删 （WeChat: 903642120）
+
+
---
+++ src/sections/Home/index.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from "react";
+import { Col, Row } from "antd";
+import Fade from "react-reveal/Fade";
+import styles from "./styles.module.scss";
+import useTyped from "../../hooks/useTyped";
+import useLottie from "../../hooks/useLottie";
+import { arrowDownLottie, sheepLottie } from "./constants";
+
+const strings = ["聊聊技术。", "吹吹水。", "打打游戏。", "唱唱歌。"];
+
+const Home: FC = () => {
+  const el = useTyped(strings, { loop: true });
+  const arrowDownLottieRef = useLottie(arrowDownLottie);
+  const sheepLottieRef = useLottie(sheepLottie);
+
+  const next = () => {
+    const $about = document.querySelector("#about");
+    if ($about) {
+      $about.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <section id="home" className={styles.home}>
+      <Row className={styles.content}>
+        <Col span={24} md={15} className={styles.intro}>
+          <p>我是海怪，</p>
+          <p>
+            目前在<span className={styles.zijie}> 字节</span>
+            <span className={styles.tiaodong}>跳动</span>
+            ，主用 <span className={styles.react}> React </span>。
+          </p>
+          <p>喜欢设计 💅，</p>
+          <p>
+            偶尔
+            <span className={styles.sometime} ref={el} />
+          </p>
+        </Col>
+
+        <Col span={24} md={9} className={styles.ballWrapper}>
+          <Fade bottom>
+            <div className={styles.ball}>
+              <div ref={sheepLottieRef} />
+            </div>
+          </Fade>
+        </Col>
+
+        <Col className={styles.next} span={24}>
+          <Fade top>
+            <div className={styles.handDown}>
+              <div ref={arrowDownLottieRef} onClick={next} />
+            </div>
+          </Fade>
+        </Col>
+      </Row>
+    </section>
+  );
+};
+
+export default Home;
